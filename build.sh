05e4401595c8d0b09d003da89cd3a1a65271bfca
mtdcy.chen <mtdcy.chen@gmail.com>
2018-12-26 16:04:49 +0800

    read options from env && print error on failure

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ source cbox.sh
 # config 
 # Note: 
 # 1. for release, it's better to build a huge bundle. but for project use, huge bundle takes too much resources
-HUGE=1
-BUILD_DEP=1
-GPL=0
-ENCODER=0
+BUILD_HUGE=${BUILD_HUGE:=1}
+BUILD_DEP=${BUILD_DEP:=1}
+BUILD_GPL=${BUILD_GPL:=0}
+BUILD_ENCODER=${BUILD_ENCODER:=0}
+BUILD_MUXER=${BUILD_MUXER:=0}
 
 # common codecs
 DEMUXERS="aac,ac3,amr,ape,apng,aptx,asf,avi,eac3,flac,flv,gif,gsm,hls,m4v,matroska,mjpeg,mov,mp3,mpegps,mpegts,ogg,vc1,wav"
@@ -26,7 +27,7 @@ WS=`pwd`
 # download packages
 PKG=$WS/packages
 mkdir -p $WS/packages
-./download.sh || exit 1
+./download.sh || { error "download packages failed"; exit 1; }
 
 # setup env
 PREFIX=$WS/prebuilts
@@ -40,7 +41,7 @@ ZLIB_ARGS="--prefix=$PREFIX"
 tar -xf $WS/packages/zlib-1.2.11.tar.gz  &&
 cd zlib-1.2.11  &&
 ./configure $ZLIB_ARGS && 
-make install 
+make install || { error "build zlib failed"; exit 1; }
 cd -
 
 # lzma - in the public domain
@@ -48,7 +49,7 @@ XZ_ARGS="--prefix=$PREFIX --disable-debug --disable-dependency-tracking --disabl
 tar -xf $WS/packages/xz-5.2.4.tar.bz2  &&
 cd xz-5.2.4  &&
 ./configure $XZ_ARGS && 
-make install
+make install || { error "build lzma failed"; exit 1; }
 cd -
 
 # libiconv - LGPL
@@ -56,31 +57,29 @@ LIBICONV_ARGS="--prefix=$PREFIX --disable-dependency-tracking --disable-debug --
 tar -xf $WS/packages/libiconv-1.15.tar.gz &&
 cd libiconv-1.15  &&
 ./configure $LIBICONV_ARGS && 
-make install
+make install || { error "build libiconv failed"; exit 1; }
 cd -
 
 fi  # BUILD_DEP
 
 # FFmpeg - GPL or LGPL
 FFMPEG_ARGS="--prefix=$PREFIX --extra-ldflags=\"-L$PREFIX/lib\" --extra-cflags=\"-I$PREFIX/include\" --enable-shared --enable-static --enable-rpath --enable-pthreads --enable-hardcoded-tables --cc=clang --host-cflags= --host-ldflags= --enable-opencl --enable-videotoolbox"
-if [ $HUGE -eq 1 ]; then 
+if [ $BUILD_HUGE -eq 1 ]; then 
     FFMPEG_ARGS+=" --enable-decoders --enable-encoders --enable-demuxers --enable-muxers"
 else
     FFMPEG_ARGS+=" --disable-decoders --disable-encoders --disable-demuxers --disable-muxers"
 
     FFMPEG_ARGS+=" --enable-decoder=$A_DECODERS,$V_DECODERS,$S_DECODERS"
 
-    if [ $ENCODER -eq 1 ]; then
-        FFMPEG_ARGS+=" --enable-encoder=$A_ENCODERS,$V_ENCODERS,$S_ENCODERS"
-    fi
+    [ $BUILD_ENCODER -eq 1 ] && FFMPEG_ARGS+=" --enable-encoder=$A_ENCODERS,$V_ENCODERS,$S_ENCODERS"
 
     FFMPEG_ARGS+=" --enable-demuxer=$DEMUXERS"
-    FFMPEG_ARGS+=" --enable-muxer=$MUXERS"
+    [ $BUILD_MUXER -eq 1 ] && FFMPEG_ARGS+=" --enable-muxer=$MUXER"
 fi
 info "ARGS: $FFMPEG_ARGS"
 
 tar -xf $WS/packages/ffmpeg-4.1.tar.bz2 &&
 cd ffmpeg-4.1 &&
 ./configure $FFMPEG_ARGS &&
-make install
+make install || { error "build ffmpeg failed"; exit 1; }
 cd -
